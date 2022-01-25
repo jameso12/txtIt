595c9fade089397e7727b9fe5441b82fad7635c9
James Rodriguez <jamesorodz@gmail.com>
2022-01-24 22:54:57 -0400

    blog/getPost is done

--- appT/blog.py
+++ appT/blog.py
@@ -36,9 +36,28 @@ def create():
 def update():
     return render_template("blog/update.html")
 
-# @bp.route('/create', methods = ('GET','POST'))
-def delete():
-    pass
+@bp.route('/<int:id>/update', methods=('GET', 'POST'))
+@loadLoggedUser
+def update(id):
+    post = getPost(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            with current_app:
+                db = create_engine(current_app.config['DATABASE'])
+                db.execute(f'UPDATE posts SET title = "{title}", body = "{body}" WHERE id = {id}' )
+            return redirect(url_for('blog.index'))
+
+    return render_template('blog/update.html', post=post)
 
 @bp.route('/<int:id>/delete', methods=('POST',))
 @loadLoggedUser
@@ -49,6 +68,15 @@ def delete(id):
         db.execute(f'DELETE FROM posts WHERE id = {id}')
     return redirect(url_for('blog.index'))
 # @bp.route('/create', methods = ('GET','POST'))
-def getPost():
-    pass
+def getPost(id, check_author=True):
+    with current_app:
+        db = create_engine(current_app.config['DATABASE'])
+        post = db.execute(f'SELECT posts.id, title, body, created, author_id, username FROM post JOIN user ON posts.author_id = users.id WHERE posts.id = {id}').fetchone()
 
+    if post is None:
+        abort(404, f"Post id {id} doesn't exist.")
+
+    if check_author and post[4] != g.user[0]:
+        abort(403)
+
+    return post
