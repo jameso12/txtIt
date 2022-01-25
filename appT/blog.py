8d9cd2236bb19a8aacd01834bfffd3aa39ebbda9
James Rodriguez <jamesorodz@gmail.com>
2022-01-24 22:38:25 -0400

    i think blog/create is done

--- appT/blog.py
+++ appT/blog.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, flash, g, redirect, render_template, request, url_for, current_app
 from sqlalchemy import create_engine
 from werkzeug.exceptions import abort
-from appT.auth import login_required
+from appT.auth import loadLoggedUser
 
 bp = Blueprint('blog', __name__)
 
@@ -13,15 +13,33 @@ def index():
             username FROM posts JOIN user ON posts.author_id = users.id ORDER BY dateCreated DESC").fetchall()
     return render_template('blog/index.html', posts=posts)
 
+@bp.route('/create', methods = ('GET','POST'))
+@loadLoggedUser
 def create():
-    pass
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
 
+        if not title:
+            error = "Title is required"
+        if error is not None:
+            flash(error)
+        else:
+            with current_app:
+                db = create_engine(current_app.config['DATABASE'])
+                db.execute(f'INSERT INTO post (title, body, author_id) VALUES ("{title}", "{body}", {g.user[0]}')
+            redirect(url_for('blog.index'))
+    return render_template("blog/create.html")
+
+@bp.route('/update', methods = ('GET','POST'))
 def update():
-    pass
+    return render_template("blog/update.html")
 
+# @bp.route('/create', methods = ('GET','POST'))
 def delete():
     pass
-
+# @bp.route('/create', methods = ('GET','POST'))
 def getPost():
     pass
 
